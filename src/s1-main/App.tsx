57af125a6bb1cb6c66fea3d346cdfa616555e2b3
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-02-23 11:56:33 +0300

    => saved

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import s from './App.module.css'
-import HW6 from '../s2-homeworks/hw06/HW6'
+import HW5 from '../s2-homeworks/hw05/HW5'
 
 function App() {
     return (
@@ -12,8 +12,7 @@ function App() {
             {/*<HW4/>*/}
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
-            {/*<HW5/>*/}
-            <HW6/>
+            <HW5/>
         </div>
     )
 }
