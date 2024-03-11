1b0fefaa8dcd8a903a075bb68b1f92ae63cb81de
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-03-11 21:12:20 +0300

    => saved

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -6,36 +6,28 @@ import s from './Clock.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number | undefined>(undefined)
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
     const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
     const [show, setShow] = useState<boolean>(false)
 
     const start = () => {
-        // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
-        // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-
         const timerId = setInterval(() => {
             setDate(new Date())
         }, 1000)
-
         setTimerId(+timerId)
-
     }
 
     const stop = () => {
-        // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-
         clearInterval(timerId)
         setTimerId(undefined)
-
     }
 
-    const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
+    const onMouseEnter = () => {
         setShow(true)
     }
-    const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
+    const onMouseLeave = () => {
         setShow(false)
     }
+
 
     function formatTime(date: Date) {
         const hours = date.getHours()
@@ -52,37 +44,43 @@ function Clock() {
     function formatDayOfWeek(date: Date) {
         const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
         const dayIndex = date.getDay()
-
         return daysOfWeek[dayIndex - 1]
     }
 
     function formatMonth(date: Date) {
         const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
         const monthIndex = date.getMonth()
-
         return months[monthIndex]
     }
 
     function formatDate(date: Date) {
         const day = String(date.getDate()).padStart(2, '0')
-        const month = String(date.getMonth() + 1).padStart(2, '0') // добавляем 1, чтобы получить реальный номер месяца
+        const month = String(date.getMonth() + 1).padStart(2, '0')
         const year = date.getFullYear()
-
         return `${day}.${month}.${year}`
     }
 
-    // const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringTime = formatTime(date)
+    //ПРАВИЛЬНЫЙ ВАРИАНТ
 
-    //const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
-    const stringDate = formatDate(date)
+    const formatterTime = new Intl.DateTimeFormat('en-US', {
+        hour12: false,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    })
+    const stringTime = formatterTime.format(date)
 
-    // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    // const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringDay = formatDayOfWeek(date)
 
-    // const stringMonth = 'date->month' || <br/> // пишут студенты
-    const stringMonth = formatMonth(date)
+    const formatterDate = new Intl.DateTimeFormat('en-US', {day: '2-digit', month: '2-digit', year: 'numeric'})
+    const stringDate = formatterDate.format(date)
+
+
+    const formatterDay = new Intl.DateTimeFormat('en-US', {weekday: 'long'})
+    const stringDay = formatterDay.format(date)
+
+
+    const formatterMonth = new Intl.DateTimeFormat('en-US', {month: 'long'})
+    const stringMonth = formatterMonth.format(date)
 
     return (
         <div className={s.clock}>
